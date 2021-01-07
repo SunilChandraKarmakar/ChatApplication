8c7930ba739d00e598ed84a0384912785eeb4b35
Sunil <sunil_karmakar@ymail.com>
2021-01-07 17:31:39 +0600

    Working with ChatApplication.Models
    1. Create Account Model.
    2. Create Message Model.

---
+++ ChatApplication/ChatApplication.Models/Model/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ChatApplication.Models.Model
+{
+    public class Account
+    {
+        public Account()
+        {
+            Messages = new HashSet<Message>();
+        }
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Provied First Name.")]
+        [StringLength(20, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please Provied Last Name.")]
+        [StringLength(20, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please Provied Valid Email Address.")]
+        [StringLength(50, MinimumLength = 11)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public virtual ICollection<Message> Messages { get; set; }
+    }
+}
---
+++ ChatApplication/ChatApplication.Models/Model/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ChatApplication.Models.Model
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Select User Account")]
+        public int AccountId { get; set; }
+
+        [Required(ErrorMessage = "Please Provied Your Message.")]
+        [StringLength(1000, MinimumLength = 1)]
+        [DataType(DataType.MultilineText)]
+        public string MessageBody { get; set; }
+
+        public virtual Account Account { get; set; }
+    }
+}
